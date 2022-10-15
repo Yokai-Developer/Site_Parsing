90e8189035ac77146512ae26b281e2000f98035c
Виктор <redvivos@gmail.com>
2022-10-15 10:43:30 +0300

    function - input data handler

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import chromedriver_binary
-import time
 
 product_list = []
+modtoplist = []
+reslist = []
+
+def structure_list(product_list, modtoplist, reslist):
+    i = 1
+    j = 0
+    # Собираем данные полученные с сайта в слова и значения
+    while i < len(product_list):
+        if product_list[i] == '\n':
+            modtoplist.append(product_list[0])
+            product_list.pop(i)
+            product_list.pop(0)
+        else:
+            product_list[0] += product_list[i]
+            product_list.pop(i)
+    product_list.clear()
+    # Структурируем данные в удобный и читаемый вид по индексам
+    while j < len(modtoplist):
+        if len(modtoplist[j]) > 20:
+            if modtoplist[j] != 0:
+                if len(modtoplist[j - 2]) == 3:
+                    layout = modtoplist[j + 2] + ' ' + modtoplist[j - 2] + ' ' + modtoplist[j] + ' ' + \
+                             modtoplist[j + 3] + modtoplist[j + 4]
+                else:
+                    layout = modtoplist[j + 2] + ' ' + modtoplist[j] + ' ' + \
+                             modtoplist[j + 3] + modtoplist[j + 4]
+                reslist.append(layout)
+        j += 1
+    return print(reslist)
+
 
 browser = webdriver.Chrome()
 browser.get('https://www.citilink.ru/catalog/chistyaschie-sredstva--pnevmaticheskie-ochistiteli/')
@@ -13,5 +42,6 @@ for card in cards:
     product_list.append(card)
 print(product_list)
 
-time.sleep(2)
+structure_list(product_list, modtoplist, reslist)
+
 browser.close()
